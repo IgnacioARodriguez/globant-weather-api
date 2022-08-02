bcc7426d66c43b1e95c6582ae9a5de6c2c7d870f
ignacio.rodriguez3 <ignacio.rodriguez@balloon-group.com>
2022-08-02 15:28:15 -0300

    fix(responses): fixed responses in controller api endpoint

--- weather_project/weather_api/views.py
+++ weather_project/weather_api/views.py
@@ -8,11 +8,14 @@ from weather_api.services import WeatherApiServices
 
 @api_view(['GET'])
 def get_weather(request):
-    city = request.query_params['city']
-    country = request.query_params['country']
-    try:
-        weather_data = WeatherApiServices(
-            city, country).get_current_weather_data()
-        return Response(weather_data)
-    except Exception as e:
-        return Response(e.args[0])
+    if request.query_params.get('city') is None or request.query_params.get('country') is None:
+        return Response({'error': 'Please provide city and country'})
+    else:
+        city = request.query_params['city']
+        country = request.query_params['country']
+        try:
+            weather_data = WeatherApiServices(
+                city, country).get_current_weather_data()
+            return Response(weather_data)
+        except Exception as e:
+            return Response(str(e))
